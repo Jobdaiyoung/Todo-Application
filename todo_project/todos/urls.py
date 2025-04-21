4c0fdf8a450853dfe641f5c3098e5f1d47c33e8e
Jirapat CHUTIMANTAPONG <jirapat.chu@ku.th>
2025-04-21 21:26:10 +0700

    make homepage the login page

--- todo_project/todos/urls.py
+++ todo_project/todos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.todo_list, name='todo_list'),
+    path('todo/', views.todo_list, name='todo_list'),
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('add/', views.add_todo, name='add_todo'),
     path('delete/<int:todo_id>/', views.delete_todo, name='delete_todo'),
